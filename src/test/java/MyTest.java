1568a73f8db9712bc5e25c0a54f30f152671bfc4
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-16 20:03:04 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,16 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testHelloWorld() throws Exception {
+        assertEquals("hello world!", HelloWorld.greet());
+    }
 
+    private static class HelloWorld {
+        public static String greet() {
+            return "hello world!";
+        }
     }
 }
